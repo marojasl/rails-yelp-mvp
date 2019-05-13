728e5be009596fde13edfea284525acdfbf71710
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-13 16:20:34 +0100

    Restaurants and reviews CRUD pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    # set the id of restaurant to the review
+    # @review.restaurant_id = params[:restaurant_id]
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # /restaurants/:id
+    resources :reviews, only: [:new, :create, :show]
+    # /restaurants/:id/review/new
+  end
+  # resources :review, only: [:new, :create]
+  # reviews/new
 end
